c0599aa3bbb668c5c6e9011f34f53c5c8fb99064
Jeroen Mulder <jeroen.mulder.1989@gmail.com>
2016-12-16 13:40:43 +0100

    finished 01math.php in assignment 6 of PHP basics.

--- 01math.php
+++ 01math.php
@@ -1,1 +1,35 @@
 <?php
+    //check to see if field is set
+    if(isset($_POST['radius'])){
+        //calculation
+        $output = 2 *$_POST['radius'] * pi();
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <style type="text/css">
+        body{
+                font-family: sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form action="01math.php" method="post">
+        <b>Voer de straal van de cirkel  in:</b>
+        <br/>
+        <input type="text" name="radius" value="
+                   <?php
+                        //remebers the user's input.
+                        if(isset($_POST['radius'])) { echo $_POST['radius']; }
+                    ?>
+        "/>
+        <br/>
+        <input type="submit" name="button" value="Submit">
+    </form>
+<?php
+    echo '<br/>'.$output;
+?>
+</body>
+</html>
+
